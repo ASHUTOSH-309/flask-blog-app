83761ab02f3fc67a8b6e8f205d8d1f94ee0e2b0a
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2025-03-12 13:44:05 +0530

    Dummy posts rendered using loop control inside template

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,17 @@ from flask import render_template
 
 def index():
     user={'username':'Ashutosh'}
-    return render_template('index.html',user=user)
+
+    posts=[
+
+        {
+            'author':{'username':'John'},
+            'body':'Hi, I am john cena, gonna beat brock lesnar today'
+        },
+        {
+            'author':{'username':'anonymous'},
+            'body': "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius pariatur at amet nesciunt mollitia, consequuntur aliquid repellat magnam?"
+        },
+      
+    ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
